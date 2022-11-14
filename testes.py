2c5ee1ea0f3f457be002d6161e59c92df08ad261
Rodrigo Temporim <juniortemporim@gmail.com>
2022-11-14 00:23:00 -0300

    v1

--- testes.py
+++ testes.py
@@ -7,12 +7,17 @@ import pandas as pd
 
 def openFile():
    filepath = filedialog.askopenfilename()
-   dinheiro_df = pd.read_excel(filepath)  
-   print(dinheiro_df) 
-   print(dinheiro_df.describe())
-   # file = open(filepath, 'r')   
-   # print(file.read())
-   # file.close()
+   dinheiro_df = pd.read_excel(filepath)   
+   dinheiro_df['Histórico'] = dinheiro_df['Histórico'].str.replace('\D+', '')   
+   seleta = dinheiro_df.dropna(how='all')
+   print(seleta)
+    
+   
+   
+#    notas = dinheiro_df['Histórico']
+#    # file = open(filepath, 'r')   
+#    # print(file.read())
+#    # file.close()
    
 
 window = Tk()
