94a7c2e2e93df944a904f60bbc23e778529bcb94
Rodrigo Temporim <juniortemporim@gmail.com>
2022-11-15 21:26:11 -0300

    comentários !

--- testes.py
+++ testes.py
@@ -5,11 +5,11 @@ from tkinter import filedialog
 import pandas as pd
 
 
-def openFile():
-   filepath = filedialog.askopenfilename()
-   dinheiro_df = pd.read_excel(filepath)   
-   dinheiro_df['Histórico'] = dinheiro_df['Histórico'].str.replace('\D+', '')   
-   seleta = dinheiro_df.dropna(how='all')
+def openFile(): # função pra de abrir a janela
+   filepath = filedialog.askopenfilename() #vai selecionar o path do arquivo selecionado
+   dinheiro_df = pd.read_excel(filepath)   # vai ler o excel no path encontrado no anterior
+   dinheiro_df['Histórico'] = dinheiro_df['Histórico'].str.replace('\D+', '')   # vai limnpar a coluna historico  mantendo apenas o codigo das contas
+   seleta = dinheiro_df.dropna(how='all') # vai limpar a coluna historico eliminando todos as linhas completamente vazias 
    print(seleta)
     
    
@@ -20,7 +20,7 @@ def openFile():
 #    # file.close()
    
 
-window = Tk()
-button = Button(text="Open", command=openFile)
+window = Tk() # coisa do tkinter pra abrir a janela
+button = Button(text="Open", command=openFile) # ela abre aqui quando apertar o botão no command!!!!
 button.pack()
-window=mainloop()
+window=mainloop() # mantém em loop
