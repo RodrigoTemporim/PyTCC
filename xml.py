1b35cf3fadf88d49c6e5d6e67fdca974d90a11b5
Rodrigo Temporim <88063503+RodrigoTemporim@users.noreply.github.com>
2023-02-02 20:45:40 -0300

    Delete xml.py

--- xml.py
+++
@@ -1,5 +0,0 @@
-import untangle
-
-document = untangle.parse('C:/Program Files (x86)/Steam/steamapps/common/Hunt Showdown/user/profiles/default/attributes.xml')
-
-input('Digite o nome da conta: \n')
